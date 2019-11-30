16f7e490aa0d22dc52e4d0d626f689ece5d0cda5
BaLiKfromUA <yuhimenko.valentin@gmail.com>
2019-11-30 13:59:41 +0200

    user data todo:up api version

--- app/src/main/java/com/memesAndPunkRock/fakeInst/api/data/UserData.java
+++ app/src/main/java/com/memesAndPunkRock/fakeInst/api/data/UserData.java
@@ -41,15 +41,89 @@ public class UserData {
      **/
     private Float hasExternalUrl;
 
-    /**
-     * private or not
-     **/
-    private Float isPrivate;
-
     private Float postsNumber;
 
     private Float followersNumber;
 
     private Float followsNumber;
 
+    public Float getIsUserWithoutAvatar() {
+        return isUserWithoutAvatar;
+    }
+
+    public void setIsUserWithoutAvatar(Float isUserWithoutAvatar) {
+        this.isUserWithoutAvatar = isUserWithoutAvatar;
+    }
+
+    public Float getRatioOfNumbersCharsToUsernameLength() {
+        return ratioOfNumbersCharsToUsernameLength;
+    }
+
+    public void setRatioOfNumbersCharsToUsernameLength(Float ratioOfNumbersCharsToUsernameLength) {
+        this.ratioOfNumbersCharsToUsernameLength = ratioOfNumbersCharsToUsernameLength;
+    }
+
+    public Float getFullNameTokensNumber() {
+        return fullNameTokensNumber;
+    }
+
+    public void setFullNameTokensNumber(Float fullNameTokensNumber) {
+        this.fullNameTokensNumber = fullNameTokensNumber;
+    }
+
+    public Float getRatioOfNumbersCharsToFullNameLength() {
+        return ratioOfNumbersCharsToFullNameLength;
+    }
+
+    public void setRatioOfNumbersCharsToFullNameLength(Float ratioOfNumbersCharsToFullNameLength) {
+        this.ratioOfNumbersCharsToFullNameLength = ratioOfNumbersCharsToFullNameLength;
+    }
+
+    public Float getIsUsernameEqualsFullname() {
+        return isUsernameEqualsFullname;
+    }
+
+    public void setIsUsernameEqualsFullname(Float isUsernameEqualsFullname) {
+        this.isUsernameEqualsFullname = isUsernameEqualsFullname;
+    }
+
+    public Float getDescriptionLength() {
+        return descriptionLength;
+    }
+
+    public void setDescriptionLength(Float descriptionLength) {
+        this.descriptionLength = descriptionLength;
+    }
+
+    public Float getHasExternalUrl() {
+        return hasExternalUrl;
+    }
+
+    public void setHasExternalUrl(Float hasExternalUrl) {
+        this.hasExternalUrl = hasExternalUrl;
+    }
+
+    public Float getPostsNumber() {
+        return postsNumber;
+    }
+
+    public void setPostsNumber(Float postsNumber) {
+        this.postsNumber = postsNumber;
+    }
+
+    public Float getFollowersNumber() {
+        return followersNumber;
+    }
+
+    public void setFollowersNumber(Float followersNumber) {
+        this.followersNumber = followersNumber;
+    }
+
+    public Float getFollowsNumber() {
+        return followsNumber;
+    }
+
+    public void setFollowsNumber(Float followsNumber) {
+        this.followsNumber = followsNumber;
+    }
 }
--- app/src/main/java/com/memesAndPunkRock/fakeInst/api/impl/InstApiControllerImpl.java
+++ app/src/main/java/com/memesAndPunkRock/fakeInst/api/impl/InstApiControllerImpl.java
@@ -5,12 +5,19 @@ import com.memesAndPunkRock.fakeInst.api.data.UserData;
 import com.memesAndPunkRock.fakeInst.exception.UserNotFoundException;
 
 import org.jinstagram.Instagram;
+import org.jinstagram.entity.users.feed.UserFeed;
+import org.jinstagram.entity.users.feed.UserFeedData;
+import org.jinstagram.exceptions.InstagramException;
+
+import java.util.List;
 
 /**
  * @author BaLiK
  *
  * */
 public class InstApiControllerImpl implements InstApiController {
+    private static final String TAG = "INST_API";
+
     private final String ACCESS_TOKEN = "4779711016.1677ed0.a6ef4ef0fb6649f18b835f8ac15e2b65";
     private Instagram instagram = null;
 
@@ -18,9 +25,37 @@ public class InstApiControllerImpl implements InstApiController {
         this.instagram = new Instagram(ACCESS_TOKEN);
     }
 
+    private UserData convertUserFeedDataToUserData(UserFeedData feedData) {
+        if (feedData == null){
+            return null;
+        }
+
+        UserData convertedData = new UserData();
+        float numberOfNumChars = feedData.getUserName().chars()
+                                                        .filter(Character::isDigit)
+                                                        .count();
+        convertedData.setRatioOfNumbersCharsToUsernameLength(numberOfNumChars / feedData.getUserName().length());
+        //todo:
+        return convertedData;
+    }
 
     @Override
     public UserData getUserDataByUserName(String username) throws UserNotFoundException {
-        return null;
+        try {
+            if(username == null || username.isEmpty()){
+                throw new InstagramException("invalid username");
+            }
+
+            UserFeed userFeed = instagram.searchUser(username);
+            List<UserFeedData> users = userFeed.getUserList();
+            if (users.isEmpty()){
+                throw new InstagramException("user list is empty");
+            }
+
+            return convertUserFeedDataToUserData(users.get(0));
+        } catch (InstagramException e) {
+            throw new UserNotFoundException("Search user error. Reason:\n"+e.getMessage());
+        }
     }
+
 }
